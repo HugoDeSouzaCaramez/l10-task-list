bb02c3eadc0767839cab60369c9e2711429cdcdd
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2023-10-08 20:43:13 -0300

    entendendo blade template

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'Main Page';
+    return view('index', [
+        'name' => 'Hugo'
+    ]);
 });
 
 Route::get('/hello', function () {
