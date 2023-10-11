a1ea69c58c21c1bf400896b5078bb9da9453fa07
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2023-10-11 19:31:42 -0300

    alterando o estado da task

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -12,8 +12,30 @@
     <p>{{$task->created_at}}</p>
     <p>{{$task->updated_at}}</p>
 
+    <p class="mb-4">
+    @if ($task->completed)
+      <span class="font-medium text-green-500">Completed</span>
+    @else
+      <span class="font-medium text-red-500">Not completed</span>
+    @endif
+  </p>
+
     <div>
-        <form action="{{route('tasks.destroy', ['task' => $task->id])}}" method="POST">
+        <a href="{{route('tasks.edit', ['task' => $task])}}">Edit</a>
+    </div>
+
+    <div>
+    <form method="POST" action="{{ route('tasks.toggle-complete', ['task' => $task]) }}">
+      @csrf
+      @method('PUT')
+      <button type="submit" class="btn">
+        Mark as {{ $task->completed ? 'not completed' : 'completed' }}
+      </button>
+    </form>
+    </div>
+
+    <div>
+        <form action="{{route('tasks.destroy', ['task' => $task])}}" method="POST">
             @csrf
             <!--METHOD SPOOPHING-->
             @method('DELETE')
