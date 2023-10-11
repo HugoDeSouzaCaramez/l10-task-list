3d70283c9c58f58ba1dffa0381261582ab812c51
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2023-10-11 20:33:01 -0300

    Adicionando estilo com Tailwind CSS: link cdn
    adicionado classe html condicional com a diretiva @class

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,13 +3,19 @@
 @section('title', 'The list of tasks')
 
 @section('content')
-    <div>
-        <a href="{{route('tasks.create')}}">Add Task</a>
-    </div>
+    <nav class="mb-4">
+        <a href="{{route('tasks.create')}}"
+        class="font-medium text-gray-700 underline decoration-pink-500">Add Task
+    </a>
+    </nav>
     <!--@if(count($tasks))-->
     @forelse($tasks as $task)
     <div>
-        <a href="{{route('tasks.show', ['task' => $task->id])}}">{{$task->title}}</a>
+        <!--@class classe condicional-->
+        <a href="{{route('tasks.show', ['task' => $task->id])}}"
+        @class(['line-through' =>$task->completed])>
+        {{$task->title}}
+        </a>
     </div>
     @empty
     <div>There are no tasks!</div>
@@ -17,7 +23,7 @@
     <!--@endif-->
 
     @if($tasks->count())
-        <nav>
+        <nav class="mt-4">
         {{$tasks->links()}}
         </nav>
     @endif
