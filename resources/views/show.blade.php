ee4573146bd6def043b3c5f9d021407ed186c3ef
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2023-10-11 22:43:16 -0300

    Estilizando a página de tarefas
    aplicado diretiva @apply de tailwindcss

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,14 +3,17 @@
 @section('title', $task->title)
 
 @section('content')
-    <p>{{$task->description}}</p>
+    <div class="mb-4">
+        <a href="{{route('tasks.index')}}"class="link">← Go back to the task list!</a>
+    </div>
+
+    <p class="mb-4 text-slate-700">{{$task->description}}</p>
 
     @if($task->long_description)
-        <p>{{$task->long_description}}</p>
+        <p class="mb-4 text-slate-700">{{$task->long_description}}</p>
     @endif
 
-    <p>{{$task->created_at}}</p>
-    <p>{{$task->updated_at}}</p>
+    <p class="mb-4 text-sm text-slate-500">Created {{ $task->created_at->diffForHumans() }} • Updated {{ $task->updated_at->diffForHumans() }}</p>
 
     <p class="mb-4">
     @if ($task->completed)
@@ -20,11 +23,10 @@
     @endif
   </p>
 
-    <div>
-        <a href="{{route('tasks.edit', ['task' => $task])}}">Edit</a>
-    </div>
-
-    <div>
+    <div class="flex gap-2">
+        <a href="{{route('tasks.edit', ['task' => $task])}}"
+        class="btn">Edit
+        </a>
     <form method="POST" action="{{ route('tasks.toggle-complete', ['task' => $task]) }}">
       @csrf
       @method('PUT')
@@ -32,14 +34,11 @@
         Mark as {{ $task->completed ? 'not completed' : 'completed' }}
       </button>
     </form>
-    </div>
-
-    <div>
-        <form action="{{route('tasks.destroy', ['task' => $task])}}" method="POST">
-            @csrf
-            <!--METHOD SPOOPHING-->
-            @method('DELETE')
-            <button type="submit">Delete</button>
-        </form>
+    <form action="{{route('tasks.destroy', ['task' => $task])}}" method="POST">
+        @csrf
+        <!--METHOD SPOOPHING-->
+        @method('DELETE')
+        <button type="submit" class="btn">Delete</button>
+    </form>
     </div>
 @endsection
